54dae450a57942efdbb9079719fba9c9522c9a31
tomerbr3456 <59730830+tomerbr3456@users.noreply.github.com>
2025-04-16 14:01:13 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-89",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+        "pandas>=1.2.0",
+        "numpy>=1.20.0",
+    ],
+    author="",
+    author_email="",
+    description="Experimental payment project using FastAPI in Python",
+    keywords="repo-89, python",
+    url="",
+)
